f433b9b4de871db12c275db2faee4fffeecaafb9
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2018-11-29 19:05:31 +0100

    First version of the MPU Generator

---
+++ Utils.py
@@ -0,0 +1,29 @@
+import os
+from Constants import *
+
+def generate_powers_array():
+    temp = []
+
+    for i in range(MIN_POWER, MAX_POWER + 1):
+        temp.append(1 << (i + 1))
+
+    return temp
+
+def delete_and_rename_file():
+    # The generated temp file has the new and correct MPU configuration
+    if os.path.isfile('..\Source\HAL\ST31_MPU_temp.c'):
+        try:
+            # This generated file can't be read protected.
+            os.remove("..\Source\HAL\ST31_MPU.c") 	
+            os.rename('..\Source\HAL\ST31_MPU_temp.c', '..\Source\HAL\ST31_MPU.c')
+
+        except OSError:
+            os.rename('..\Source\HAL\ST31_MPU_temp.c', '..\Source\HAL\ST31_MPU.c')            
+
+        finally:
+            return True
+
+    else:
+        print("Error: This tool didn't generate the new MPU configs in ST31_MPU_temp.c")
+
+        return False
